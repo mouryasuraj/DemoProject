ccfb19c9797beefaa1a84a9f0b51477870aab3f1
Suraj Mourya <2276661@gnkhalsa.edu.in>
2023-08-10 15:17:39 +0530

    Improved the menu bar

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,25 @@
+
+let menuBar = document.getElementById("menuBar");
+let closeBar = document.getElementById("closeBar");
+let bg = document.getElementById("bg");
+let menu = document.getElementById("menu");
+
+menuBar.addEventListener('click', ()=>{
+    menu.style.left = "0";
+    bg.style.display = "block";
+    
+})
+closeBar.addEventListener('click', ()=>{
+    menu.style.left = "-100%"
+    bg.style.display = "none"
+})
+
+
+
+
+
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
